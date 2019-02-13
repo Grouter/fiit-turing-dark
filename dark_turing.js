7d5ba3ce3a4377159e4f15f5a61c24f54c705391
Grouter <matus.perfekt@gmail.com>
2019-02-13 11:55:36 +0100

    js file

---
+++ dark_turing.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         DarkTuring
+// @namespace    DarkTuring
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://93.184.70.44:4001/*
+// @match        https://www.turing.sk/fiit/zprpr2/*
+// @grant        none
+// ==/UserScript==
+
+window.onload = function() {
+    monaco.editor.setTheme('vs-dark');
+};
