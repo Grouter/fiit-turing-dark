9abd0e08069044b51d061376e91f396b55aed585
Grouter <matus.perfekt@gmail.com>
2019-02-13 12:07:51 +0100

    @licence update

--- dark_turing.user.js
+++ dark_turing.user.js
@@ -2,11 +2,12 @@
 // @name         DarkTuring
 // @namespace    DarkTuring
 // @version      0.1
-// @description  try to take over the world!
+// @description  Welcome to the Dark Turing!
 // @author       You
 // @match        http://93.184.70.44:4001/*
 // @match        https://www.turing.sk/fiit/zprpr2/*
 // @grant        none
+// @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // ==/UserScript==
 
 window.onload = function() {
