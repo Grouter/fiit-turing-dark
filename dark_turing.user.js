2bc42770ea9a9edc73013b2859f5882135874c2f
Grouter <matus.perfekt@gmail.com>
2019-02-13 12:11:27 +0100

    js metadata update

--- dark_turing.user.js
+++ dark_turing.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         DarkTuring
-// @namespace    DarkTuring
 // @version      0.1
 // @description  Welcome to the Dark Turing!
-// @author       You
+// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @match        http://93.184.70.44:4001/*
 // @match        https://www.turing.sk/fiit/zprpr2/*
-// @grant        none
-// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // ==/UserScript==
+
+// ==OpenUserJS==
+// @author Grouter
+// ==/OpenUserJS==
 
 window.onload = function() {
     monaco.editor.setTheme('vs-dark');
