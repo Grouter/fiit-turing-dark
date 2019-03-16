372ddd2fa5b4ebe7f41482516e4ce0822a5808b3
Grouter <matus.perfekt@gmail.com>
2019-03-17 00:31:59 +0100

    added headers for css

--- dark_turing.user.js
+++ dark_turing.user.js
@@ -2,9 +2,10 @@
 // @name         DarkTuring
 // @version      0.1
 // @description  Welcome to the Dark Turing!
+// @author       Mathew Chovan
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @match        http://93.184.70.44:4001/*
-// @match        https://www.turing.sk/fiit/zprpr2/*
+// @match        https://www.turing.sk/fiit/*
 // ==/UserScript==
 
 // ==OpenUserJS==
